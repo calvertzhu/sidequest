f4a33f45b18e5fd66ff2366aa2689f403792bc20
calvertzhu <zhucalvert@gmail.com>
2025-07-19 14:00:48 -0400

    setting up API pipeline

---
+++ backend/app/models/user.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, name, email, interests, location, travel_dates):
+        self.name = name
+        self.email = email
+        self.interests = interests
+        self.location = location
+        self.travel_dates = travel_dates
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'email': self.email,
+            'interests': self.interests,
+            'location': self.location,
+            'travel_dates': self.travel_dates
+        } 
