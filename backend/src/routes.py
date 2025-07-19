6155d8b2c6af3899ec8598a1955e75ea7c786ab2
atkhan17 <arsalantkhan17@gmail.com>
2025-07-19 13:43:42 -0400

    flask + mongodb

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -1,23 +1,37 @@
-from flask import Blueprint, request, jsonify
-from db import tasks_collection
-from bson import ObjectId
+from flask import Blueprint, request, jsonify, current_app
+from datetime import datetime
 
-api = Blueprint('api', __name__)
+users_bp = Blueprint('users', __name__)
 
-@api.route('/tasks', methods=['GET'])
-def get_tasks():
-    tasks = list(tasks_collection.find())
-    for task in tasks:
-        task['_id'] = str(task['_id'])
-    return jsonify(tasks)
+@users_bp.route('/users', methods=['POST'])
+def create_user():
+    db = current_app.config["DB"]
+    data = request.json
 
-@api.route('/tasks', methods=['POST'])
-def add_task():
-    data = request.get_json()
-    result = tasks_collection.insert_one({'task': data['task']})
-    return jsonify({'_id': str(result.inserted_id)})
+    # Parse and sanitize input
+    try:
+        user = {
+            "name": data["name"],
+            "birthday": datetime.strptime(data["birthday"], "%Y-%m-%d"),
+            "gender": data["gender"],
+            "interests": data.get("interests", []),
+            "profile_pic": data.get("profile_pic", ""),
+            "dietary_restrictions": data.get("dietary_restrictions", "")
+        }
 
-@api.route('/tasks/<id>', methods=['DELETE'])
-def delete_task(id):
-    tasks_collection.delete_one({'_id': ObjectId(id)})
-    return jsonify({'message': 'Deleted'})
+        result = db.users.insert_one(user)
+        return jsonify({"_id": str(result.inserted_id)}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+@users_bp.route("/get_all_users", methods=["GET"])
+def get_all_users():
+    db = current_app.config["DB"]
+    users = list(db.users.find({}))
+    for user in users:
+        user["_id"] = str(user["_id"])
+        if "birthday" in user:
+            user["birthday"] = user["birthday"].strftime("%Y-%m-%d")
+    return jsonify(users), 200
