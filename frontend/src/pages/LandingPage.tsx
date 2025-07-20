9b506ca74501662420be2ba99c09c55d8090bdb1
rafatkab <rafat.kabir301@gmail.com>
2025-07-19 23:35:42 -0400

    e

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -5,12 +5,13 @@ import Features from "../components/Features";
 import Testimonials from "../components/Testimonials";
 import Footer from "../components/Footer";
 import TabNavigation from "../components/TabNavigation";
+import "../images/landscape.png";
 import axios from "axios";
 import api from "../api";
 
 const LandingPage = () => {
   return (
-    <div className="bg-gradient-to-b from-gray-900 to-gray-800 min-h-screen text-white flex flex-col">
+    <div className="from-gray-900 to-gray-800 min-h-screen text-white flex flex-col bg-cover bg-center">
       <TabNavigation activeTab="dashboard" />
       <main className="flex-1 flex flex-col">
         <HeroSection />
